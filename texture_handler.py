f295fb9f750d4ca50027ed3a6041a5d7fb5f0c41
Onur <onurvallor@gmail.com>
2021-03-13 23:22:51 -0500

    Removed Texture file and added Readme

--- texture_handler.py
+++
@@ -1,7 +0,0 @@
-from ursina import *
-
-class GrabBlockTexture():
-    def __init__(self):
-        self.grass_texture = load_texture('assets/grass.png')
-    def run_ursina(self):
-        app = Ursina()
